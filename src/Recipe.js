d4497f9e83efe1710b22320b83ccfec0503198a2
Sophia Dossland <sophia.dossland@gmail.com>
2017-06-22 15:36:18 -0700

    initial commit

---
+++ src/Recipe.js
@@ -0,0 +1,3 @@
+/**
+ * Created by sophia on 6/21/17.
+ */
---
+++ src/TableOfContents.js
@@ -0,0 +1,16 @@
+/**
+ * Created by sophia on 6/21/17.
+ */
+import React, { Component } from 'react';
+
+class Summary extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Summary;
