d68204e31ac87bf8e35cd2cfa9dac17c12822f3a
Sophia Dossland <sophia.dossland@gmail.com>
2017-06-28 22:27:43 -0700

    fix bug - do not set initial state, use this.props

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,23 +4,11 @@
 import React from 'react';
 import TimeChangeModal from './TimeChangeModal';
 
-const initialState = ({
-  id: Number(),
-  name: '',
-  prepTime: Number(),
-  cookTime: Number(),
-  servings: Number(),
-  ingredients: [],
-  steps: [],
-  //show: false
-  showModal: false,
-  activity: ''
-});
 
 class Recipe extends React.Component {
   constructor(props) {
     super(props);
-    this.state = props.recipe ? Object.assign({}, props.recipe, {showModal: false, activity: ''}) : Object.assign({}, initialState);
+    this.state = {showModal: false, activity: ''};
   };
   getOnChangeName = (e) => {
     this.setState({
@@ -60,7 +48,7 @@ class Recipe extends React.Component {
       showModal: false,
       activity: ''
     })
-  }
+  };
   render() {
     const timeDisplay = (numMins) => {
       var time = this.timeConversion(numMins);
@@ -81,14 +69,15 @@ class Recipe extends React.Component {
         </div>
       )
     };
-    var steps = this.state.steps.map((step, index) => {
+
+    var steps = this.props.recipe.steps.map((step, index) => {
       return (
         <div className="row" key={index}>
           <div className="col-xs-1 step-col">
             <span className="step-number">{index + 1}</span>
           </div>
           <div className="col-xs-11 step-col">
-            <textarea className="step" rows="auto" value={step}/*cols="90%"*/ />
+            <p contentEditable="true" className="step">{step}</p>
           </div>
         </div>
       )
@@ -97,7 +86,7 @@ class Recipe extends React.Component {
       <div className="recipe" /*col-md-8 col-xs-12*/>
         <div className="row">
           <h2>
-            <input type="text" value={this.state.name} onChange={this.getOnChangeName} />
+            <input type="text" value={this.props.recipe.name} onChange={this.getOnChangeName} />
           </h2>
         </div>
         <hr />
@@ -112,15 +101,15 @@ class Recipe extends React.Component {
           </div>
           <div className="col-xs-1 prep-sum-col vertical-line" onClick={this.openPrepTimeModal}>
             <p className="prep-label prep" name="prep">Prep</p>
-            {timeDisplay(this.state.prepTime)}
+            {timeDisplay(this.props.recipe.prepTime)}
           </div>
           <div className="col-xs-1 prep-sum-col vertical-line" onClick={this.openCookTimeModal}>
             <p className="prep-label">Cook</p>
-            {timeDisplay(this.state.cookTime)}
+            {timeDisplay(this.props.recipe.cookTime)}
           </div>
           <div className="col-xs-1 prep-sum-col">
             <label htmlFor="servings">Serves</label>
-            <input className="input-prep" type="text" name="servings" value={this.state.servings} onChange={this.getOnServingsChange} />
+            <input className="input-prep" type="text" name="servings" value={this.props.recipe.servings} onChange={this.getOnServingsChange} />
           </div>
         </div>
         <hr />
@@ -128,9 +117,9 @@ class Recipe extends React.Component {
           {steps}
         </div>
         { this.state.showModal ?
-          <TimeChangeModal prepTime={this.state.prepTime}
-                           cookTime={this.state.cookTime}
-                           activity={this.state.activity}
+          <TimeChangeModal prepTime={this.props.recipe.prepTime}
+                           cookTime={this.props.recipe.cookTime}
+                           activity={this.props.recipe.activity}
                            showModal={this.state.showModal}
                            closeModal={this.closeModal}
           />
