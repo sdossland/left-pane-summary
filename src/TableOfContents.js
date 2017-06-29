d312a677096fa6938f4c57644cf0fe9fa8b24495
Sophia Dossland <sophia.dossland@gmail.com>
2017-06-28 22:33:41 -0700

    fix bug - show individual details of all including index 0, add right arrow to recipe whilst its details are shown, styling

--- src/TableOfContents.js
+++ src/TableOfContents.js
@@ -41,31 +41,36 @@ class TableOfContents extends React.Component {
   render() {
     const recipeNames = this.props.recipes.map((recipe, index) => {
       return (
-        <div className="row recipe-list-name-row" key={index} onMouseEnter={this.getOnMouseEnter(index)}
+        <div className={this.state.key === index ? 'row row recipe-list-name-row recipe-list-name-row-focus' : 'row recipe-list-name-row'} key={index} onMouseEnter={this.getOnMouseEnter(index)}
                                                               onMouseLeave={this.getOnMouseLeave}
                                                               onClick={this.viewRecipe(index)}
         >
           <p className="recipe-list-name">{recipe.name}</p>
+          <div className="float-right">
           { this.state.hoverIndex === index && this.state.hover ?
             <div className="editDeleteBtns">
               {/*<button className="btn btn-default">Edit</button>*/}
               <button className="btn btn-default" onClick={this.handleDelete(index)}>Delete</button>
             </div>
             : null }
+          { this.state.key === index ?
+              <div className="triangle-right"></div>
+            : null }
+            </div>
         </div>
       )
     });
     return (
       <div id="tableOfContents" className="row">
-          <div id="test1" className={this.state.show ? 'col-md-4 col-xs-0' : 'col-md-4 col-md-offset-4 col-xs-12'}>
+          <div id="recipeList" className={this.state.show ? 'col-md-4 col-xs-0' : 'col-md-4 col-md-offset-4 col-xs-12'}>
             <div className="row">
               <h2>Table of Contents</h2>
             </div>
             <hr />
             {recipeNames}
           </div>
-          { this.state.show ?
-            <div id="test2" className="col-md-8 col-xs-12" >
+          { typeof this.state.key !== 'undefined' && this.state.show ?
+            <div id="recipe" className="col-md-8 col-xs-12" >
               <Recipe recipe={this.props.recipes[this.state.key]}
                       recipes={this.props.recipes}
               />
