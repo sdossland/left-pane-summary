e2fc13679be79e68914159ccd23d183b1cecb6a8
Sophia Dossland <sophia.dossland@gmail.com>
2017-06-29 15:13:44 -0700

    images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import TableOfContents from './TableOfContents';
 import './App.css';
+import mountainRange from './images/mountainRange.jpg';
+import sunset from './images/sunset.jpg';
+import waterfall from './images/waterfall.jpg';
 
 var recipes = [{
   id: 0,
@@ -11,17 +14,19 @@ var recipes = [{
   ingredients: ['1 bunch kale, torn into bite size pieces', '1 ripe tomatoe', '1/2 cup raw broccoli', '1/2 avocado', '1 stalk green onion', '1/3 cup quinoa', '3 tbsps lemmon juice', '1 tbsp extra virgin olive oil', 'freshly ground pepper (to taste)', 'red chili flakes (to taste)'],
   steps: ['Lorem ipsum cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni, ut, dignissimos.',
           'Adipisicing elit.',
-          'Tempore tempora rerum..']
+          'Tempore tempora rerum..'],
+  image: mountainRange
 }, {
   id: 1,
-  name: 'test',
+  name: 'Test',
   prepTime: 10,
   cookTime: 10,
   servings: 2,
   ingredients: ['Lorem', 'ipsum'],
   steps: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
           'Tempore tempora rerum, est autem cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni, ut, dignissimos.',
-          'Lorem ipsum dolor sit amet, consectetur adipisicing elit.']
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'],
+  image: sunset
 }, {
   id: 2,
   name: 'Lorem ipsum',
@@ -31,7 +36,8 @@ var recipes = [{
   ingredients: ['Lorem', 'ipsum'],
   steps: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
     'Tempore tempora rerum, est autem cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni, ut, dignissimos.',
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit.']
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'],
+  image: waterfall
 }];
 
 class App extends React.Component {
@@ -49,9 +55,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <div className="col-xs-12">
+        {/*<div className="col-xs-12">*/}
           <TableOfContents recipes={this.state.recipes} deleteRecipe={this.deleteRecipe} />
-        </div>
+        {/*</div>*/}
       </div>
     );
   }
