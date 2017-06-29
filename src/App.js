6f1e2acb261cdfd76dcd0e739f4ad49a4689ec87
Sophia Dossland <sophia.dossland@gmail.com>
2017-06-28 22:25:59 -0700

    data fix

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ var recipes = [{
   cookTime: 10,
   servings: 1,
   ingredients: ['1 bunch kale, torn into bite size pieces', '1 ripe tomatoe', '1/2 cup raw broccoli', '1/2 avocado', '1 stalk green onion', '1/3 cup quinoa', '3 tbsps lemmon juice', '1 tbsp extra virgin olive oil', 'freshly ground pepper (to taste)', 'red chili flakes (to taste)'],
-  steps: ['Lorem', 'ipsum...'],
-  //show: false
+  steps: ['Lorem ipsum cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni, ut, dignissimos.',
+          'Adipisicing elit.',
+          'Tempore tempora rerum..']
 }, {
   id: 1,
   name: 'test',
@@ -18,8 +19,9 @@ var recipes = [{
   cookTime: 10,
   servings: 2,
   ingredients: ['Lorem', 'ipsum'],
-  steps: ['Lorem', 'ipsum...'],
-  //show: false
+  steps: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          'Tempore tempora rerum, est autem cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni, ut, dignissimos.',
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit.']
 }, {
   id: 2,
   name: 'Lorem ipsum',
@@ -27,8 +29,9 @@ var recipes = [{
   cookTime: 10,
   servings: 2,
   ingredients: ['Lorem', 'ipsum'],
-  steps: ['Lorem', 'ipsum...'],
-  //show: false
+  steps: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    'Tempore tempora rerum, est autem cupiditate, corporis a qui libero ipsum delectus quidem dolor at nulla, adipisci veniam in reiciendis aut asperiores omnis blanditiis quod quas laborum nam! Fuga ad tempora in aspernatur pariatur fugit quibusdam dolores sunt esse magni, ut, dignissimos.',
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit.']
 }];
 
 class App extends React.Component {
